e1926a6c28eb2cde82e494adf2acd3f36e277267
akash41111 <104068921+akash41111@users.noreply.github.com>
2024-03-20 17:29:54 +0530

    add

---
+++ src/main/java/com/example/demopipelinepractise/DemoPipelinePractiseApplication.java
@@ -0,0 +1,14 @@
+package com.example.demopipelinepractise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPipelinePractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPipelinePractiseApplication.class, args);
+		System.out.println("Hello, Spring Boot!");
+	}
+
+}
---
+++ src/test/java/com/example/demopipelinepractise/DemoPipelinePractiseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demopipelinepractise;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPipelinePractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(2, 1 + 1);
+	}
+
+}
